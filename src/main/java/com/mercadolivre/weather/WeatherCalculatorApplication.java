dbd49f5dfd10e30d6dfdb46809bdad073c4e4a53
adrsilva100@gmail.com <adrsilva100@gmail.com>
2020-10-04 11:16:48 -0300

    config initial

---
+++ src/main/java/com/mercadolivre/weather/WeatherCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolivre/weather/WeatherCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
